ff768ad12d36216f7325653cdfe4f098aa97171b
Ahmad Magdy <amagsid@gmail.com>
2020-09-10 20:44:25 +0200

    finished the logHello exercise

--- Week1/Homework/js-exercises/logHello.js
+++ Week1/Homework/js-exercises/logHello.js
@@ -1,1 +1,12 @@
-// first JS excercise 
+'use strict'
+
+console.log('Hello world!');
+console.log('Hallo Wereld!');
+console.log('!مرحبا بالعالم');
+console.log('Hola Mundo!');
+console.log('Բարեւ աշխարհ!');
+console.log('Bonjour le monde!');
+console.log('สวัสดีชาวโลก!');
+console.log('Ahoj světe!');
+console.log('你好，世界!');
+console.log('salve, orbis terrarum!');
