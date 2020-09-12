6b4845413ffa6b88914769818a4682507e9efebc
Ahmad Magdy <amagsid@gmail.com>
2020-09-12 08:37:54 +0200

    finished JS homework exercises for week 1

--- Week1/Homework/js-exercises/3. logNumber.js
+++ Week1/Homework/js-exercises/3. logNumber.js
@@ -2,15 +2,15 @@
 
 // exercise 3 : log the number
 
-// step 1
+// step 1 declare your variable numberX. Do not initialize it 
 let numberX;
-// step 2
+// step 2  declare your variable numberX. Do not initialize it 
 console.log('I think value of x is undefined');  //I initially wrote a random number instead of "undefined" here but then it didn't make sense to me since we are asked to write another statement of what we think the value is in step number 5
-// step 3
+// step 3 Add a console.log statement that logs the value of numberX.
 console.log(numberX);
-// step 4
+// step 4 initialize variable numberX with a number
 numberX = 42;
-// step 5
+// step 5 add a console.log statement that explains what you think the value of numberX is
 console.log("looks like the value of x is 42 'pwdlstouchalso called an integer'");
-// step 6
+// step 6 Add a console.log statement that logs the value of numberX
 console.log(numberX);
--- Week1/Homework/js-exercises/4. logString.js
+++ Week1/Homework/js-exercises/4. logString.js
@@ -2,15 +2,15 @@
 
 // exercise 4 : log the string
 
-// step 1
+// step 1 Declare a variable myString and assign a string to it. Use your full name, including spaces, as the content for the string.
 let myString = 'Ahmad ' + 'Magdy ' + 'Saeed';
-// step 2
+// step 2 Write a console.log statement in which you explain in words what you think the value of the string is.
 console.log("I think value of the myString is Ahmad Magdy Saeed"); 
-// step 3
+// step 3 console.log the variable myString.
 console.log(myString);
-// step 4
+// step 4 reassign to the variable myString a new string.
 myString = "This string will replace my full name";
-// step 5
+// step 5 write a console.log statement that explains in words what you think will be logged to the console.
 console.log("value would be'This string will replace my full name'");
-// step 6
+// step 6 console.log myString again.
 console.log(myString);
