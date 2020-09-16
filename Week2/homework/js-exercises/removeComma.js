90cfbf5f9cf87da1387ed5d5f00ff58bbda3b246
Ahmad Magdy <amagsid@gmail.com>
2020-09-16 13:47:21 +0200

    finished exercise 1/5

--- Week2/homework/js-exercises/removeComma.js
+++ Week2/homework/js-exercises/removeComma.js
@@ -1,4 +1,18 @@
 'use strict'
 
-
+// Step 1 add the variable to your file.
 let myString = 'hello,this,is,a,difficult,to,read,sentence';
+
+// Step 2 Log the length of myString.
+console.log(myString.length);
+
+
+// Step 3 remove the commas from the string and replace them with spaces.
+myString = myString.replace(/,/g, ' ');
+
+// Anogher method using the .split and .join methods
+myString = myString.split(',').join(' ');
+
+
+// Step 4 After replacing the commas, log myString to see if you succeeded.
+console.log(myString);
