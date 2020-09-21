5587b7cf922842950e53910db2c42d94fb72cea1
Ahmad Magdy <amagsid@gmail.com>
2020-09-21 16:34:36 +0200

    Completed the JS exercises

--- Week2/homework/js-exercises/readingList.js
+++ Week2/homework/js-exercises/readingList.js
@@ -1,51 +1,53 @@
 'use strict'
 
      // Step 1 Declare a variable that holds an array of 3 objects, where each object describes a book and has properties for the title (string), author (string), and alreadyRead (boolean indicating if you read it yet).
-let books  = [
+
+
+     let books  = [
     {
       title: 'The God Delusion',
       author: 'Richard Dawkins',
       alreadyRead: false,
-
     },
 
     {
         title: 'The Subtle Art of Not Giving a Fuck',
         author: 'Mark Manson',
         alreadyRead: true,
-  
       },
 
       {
         title: 'The Selfish Gene',
         author: 'Richard Dawkins',
         alreadyRead: false,
-    
       },
       
-    ];
+    ]
+ 
+    //console.log(books[0].alreadyRead)
+    
     // Step 2 Loop through the array of books.
+    for (let i=0; i < books.length; i++) {
+    //console.log(books[i])
+    
     // Step 3 For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
+    }
+
     books.forEach((book)=>console.log(book.title + ' by ' + book.author));
-    
-   
-    
+
     // Step 4 Create a conditional statement to change the log depending on whether you read it yet or not. If you read it, log a string like You already read "The Hobbit" right after the log of the book details
-   //Object.keys(books).forEach(function(whatToRead) {
-      //if (books.alreadyRead == true) {
-        if (books.alreadyRead == true) {
-        console.log('you already read ' + books.title);
-        }
-        else {
-      console.log('you still need to read ' + books.title);
-     }
-    //} )
+      
+    books.forEach((book)=> book.alreadyRead ? console.log('You already read ' + book.title) : console.log('You still need to read ' + book.title))
 
+    // node readingList.js
+
+
+    //this is was my first trial trying to create the conditional statement
+// if (books.alreadyRead === true ) {
+   //   console.log('you already read ' + books.title);
+   // }
+     //   else {
+     // console.log('you still need to read ' + books.title);
+    // }
     // The program here gives me "you still need to read undefined" as a result for all three results. 
-     // node readingList.js
-
-
-
-     //console.log(
-     // books.alreadyRead ? 'You already read' : 'You still need to read',
-     //// );
+ 
--- Week2/homework/js-exercises/wantsDrink.js
+++ Week2/homework/js-exercises/wantsDrink.js
@@ -1,21 +1,30 @@
 'use strict'
+// step 1 Declare a variable that holds an empty array, called drinkTray.
+let drinkTray = [];
+const drinkTypes = ['cola', 'lemonade', 'water'];
+let count = 0;
 
-     // Step 1 Declare a variable that holds an array of 3 objects, where each object describes a book and has properties for the title (string), author (string), and alreadyRead (boolean indicating if you read it yet).
+// step 2 Create a loop that runs 5 times. On each iteration, push a drink into the drinkTray variable. The drinkTray can only hold at most two instances of the same drink type, for example it can only hold 2 colas, 2 lemonades, 2 waters.
+for (let i = 0; i < 5; i++) {
+    //console.log(i)
+    if (i % 2 === 0) {
+        count++
+    }
+    drinkTray.push(drinkTypes[count-1])
+};
 
-
-     let drinkTray = [];
-
-     const drinkTypes = ['cola', 'lemonade', 'water'];
-
-
-
-     for (let i = 0; i < 5; i++ ) {
-         console.log(drinkTray)
-      
-     }
-
-
-    
+console.log('Hey guys, I brought a ' + drinkTray + '!' );
 
 
-     //node wantsDrink.js
+//Another solution
+
+//* const drinkTypes = ['cola', 'lemonade', 'water'];
+// for (let i = 0; i < 5; i++ ) {
+// for (let j =0; j <=1; j++) {
+// if (drinkTray.length < 5) {
+//        drinkTray.push(drinkTypes[i])
+//       }
+//      }
+//    }; 
+
+
