bbcb62fc9803f6df596321ee169f5e730808364b
Ahmad Magdy <amagsid@gmail.com>
2020-09-25 09:42:17 +0200

    finished exercise 1

---
+++ Week3/Homework/js-exercises/1.giveCompliment.js
@@ -0,0 +1,15 @@
+'use strict'
+
+//step 1
+function giveCompliment(name) {
+    let compliments = ['great', 'awesome', 'perfect', 'the boss', 'the man', 'outstanding', 'one of kind!', 'Wonderful', 'beautiful', 'Excellent' ]
+    let randomCompliment = compliments[Math.floor(Math.random() * compliments.length)];
+    
+    
+    console.log(randomCompliment);
+    return `you are `+ randomCompliment +',' + name
+    
+    
+    }
+    
+     console.log(giveCompliment('yuri'))
