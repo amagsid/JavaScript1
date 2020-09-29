99750ddcc565fac8c7a1ec84e6908a1a5122d22f
Ahmad Magdy <amagsid@gmail.com>
2020-09-29 17:31:31 +0200

    fixed return statements in addToShoppingCart.js

--- Week3/Homework/js-exercises/4. addToShoppingCart.js
+++ Week3/Homework/js-exercises/4. addToShoppingCart.js
@@ -3,24 +3,25 @@
 // Create an array called shoppingCart that holds the following strings: "bananas" and "milk"
 
 let shoppingCart = ['milk', 'banana' ]
+
 //  Write a function named addToShoppingCart
-  function addToShoppingCart(item) {
-    /* Add grocery item to shoppingCart. If the amount of items is more than 3 remove the first one in the array
-    Loops through the array in order to list out the items */
-     if (shoppingCart.length <= 2){
-        shoppingCart.push(item)
-//Return a string: "You bought [LIST_OF_GROCERY_ITEMS]!"
-         console.log('You bought '+ shoppingCart + '!');
-     } else if (shoppingCart.length <= 3){
-        shoppingCart.shift();
-        shoppingCart.push(item)
-         console.log('You bought ' + shoppingCart+'!')
-     } 
- }
+function addToShoppingCart(item) {
+  /* Add grocery item to shoppingCart. If the amount of items is more than 3 remove the first one in the array
+  Loops through the array in order to list out the items */
+  if (shoppingCart.length <= 2){
+    shoppingCart.push(item)
+    //Return a string: "You bought [LIST_OF_GROCERY_ITEMS]!"
+    return 'You bought '+ shoppingCart + '!'
+  
+} else if (shoppingCart.length <= 3){
+  shoppingCart.shift();
+  shoppingCart.push(item)
+  return 'You bought ' + shoppingCart+'!'
+ } 
+};
 
- addToShoppingCart('peanut butter');
- addToShoppingCart('ice cream');
-
+console.log(addToShoppingCart('peanut butter'));
+console.log(addToShoppingCart('ice cream'));
 
 
   // this code is from the first attempt solving the problem. I ended up commenting it out.
